1618ec62b7f7ee9f075767accbc3e242499199c5
Cameron Childress <cameronc@sumoc.com>
2023-02-06 15:56:25 -0500

    test deploy

--- .projenrc.js
+++ .projenrc.js
@@ -1,11 +1,24 @@
 const { typescript } = require('projen');
+
+const authorName = 'Cameron Childress';
+const authorAddress = 'cameronc@sumoc.com';
+const repository = 'https://github.com/sumoinc/projen-lib';
+
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
-  name: 'projen-lib',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  name: '@sumoc/projen-lib',
+  description: 'A library of projen configurations.',
+  license: 'MIT',
+  repositoryUrl: repository,
+  repository: repository,
+  authorName,
+  author: authorName,
+  authorOrganization: true,
+  copyrightOwner: authorName,
+  authorAddress: authorAddress,
+  authorEmail: authorAddress,
+  releaseToNpm: true,
+  deps: ['projen'],
 });
+
 project.synth();
