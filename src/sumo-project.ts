135898ad1de2b105014d6ad6aab3a6f2cdc5519a
Cameron Childress <cameronc@sumoc.com>
2023-02-07 08:54:00 -0500

    switch to PNPM, force vscode option

--- src/sumo-project.ts
+++ src/sumo-project.ts
@@ -1,4 +1,5 @@
 
+import { NodePackageManager } from 'projen/lib/javascript';
 import { TypeScriptProject, TypeScriptProjectOptions } from 'projen/lib/typescript';
 
 export interface SumoProjectOptions extends TypeScriptProjectOptions {
@@ -8,26 +9,28 @@ export interface SumoProjectOptions extends TypeScriptProjectOptions {
 export class SumoProject extends TypeScriptProject {
 
   constructor(options: SumoProjectOptions) {
-    super(options);
 
-    // configure vscode
-    if (this.vscode !== undefined) {
+    super({
+      ...options,
+      vscode: true,
+      packageManager: NodePackageManager.PNPM,
+    });
 
-      // root level editor settings
-      this.vscode.settings.addSetting('editor.tabSize', 2);
-      this.vscode.settings.addSetting('editor.bracketPairColorization.enabled', true);
-      this.vscode.settings.addSetting('editor.guides.bracketPairs', 'active');
-      this.vscode.settings.addSetting('editor.rulers', [80, 120]);
+    // VSCODE: Root level editor settings
+    this.vscode!.settings.addSetting('editor.tabSize', 2);
+    this.vscode!.settings.addSetting('editor.bracketPairColorization.enabled', true);
+    this.vscode!.settings.addSetting('editor.guides.bracketPairs', 'active');
+    this.vscode!.settings.addSetting('editor.rulers', [80, 120]);
 
-      // linting
-      this.vscode.settings.addSetting('editor.codeActionsOnSave', { 'source.fixAll.eslint': true });
-      this.vscode.settings.addSetting('editor.formatOnSave', false);
-      this.vscode.settings.addSetting('editor.formatOnSave', true, 'typescript');
-      this.vscode.settings.addSetting('editor.defaultFormatter', 'dbaeumer.vscode-eslint', 'typescript');
+    // VSCODE: linting
+    this.vscode!.settings.addSetting('editor.codeActionsOnSave', { 'source.fixAll.eslint': true });
+    this.vscode!.settings.addSetting('editor.formatOnSave', false);
+    this.vscode!.settings.addSetting('editor.formatOnSave', true, 'typescript');
+    this.vscode!.settings.addSetting('editor.defaultFormatter', 'dbaeumer.vscode-eslint', 'typescript');
 
-      // extensions
-      this.vscode.extensions.addRecommendations('dbaeumer.vscode-eslint');
-    }
+    // VSCODE: extensions
+    this.vscode!.extensions.addRecommendations('dbaeumer.vscode-eslint');
+
 
   }
 
